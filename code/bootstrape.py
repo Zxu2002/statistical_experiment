9ec6844174e335e98b0bd0de4c53056a0a8a3407
Zihan <zx282@cam.ac.uk>
2024-12-03 12:13:24 +0000

    modified data generating function so it takes customized parameters

--- code/bootstrape.py
+++ code/bootstrape.py
@@ -1,5 +1,9 @@
 import numpy as np
+from total_model import TotalModel
+from generate_sample import fitting, fit_func, generate_sample
 
-from generate_sample import generate_sample, fitting, fit_func
+
 x, y = generate_sample(100000)
 mi = fitting(x, y)
+
+sample_sizes = [500, 1000, 2500, 5000, 10000]
--- code/generate_sample.py
+++ code/generate_sample.py
@@ -8,11 +8,11 @@ from iminuit import Minuit
 from iminuit.cost import ExtendedUnbinnedNLL
 
 #Given there's no analytical ppf use accept-reject method
-def generate_sample(num_sample):
+def generate_sample(num_sample,mu = 3,sigma = 0.3, beta = 1,m = 1.4,f = 0.6,lamb = 0.3,mu_b = 0,sigma_b = 2.5):
     x_grid = np.linspace(0, 5, 100)
     y_grid = np.linspace(0, 10, 100)
     X, Y = np.meshgrid(x_grid, y_grid)
-    model = TotalModel() 
+    model = TotalModel(mu, sigma, beta, m,f,lamb ,mu_b,sigma_b) 
     Z = model.total_func(Y, X)
     max_density = np.max(Z) * 1.1 
 
@@ -63,19 +63,19 @@ if __name__ == "__main__":
     print(f"time, benchmark(normal distribution generate): {t_norm}")
     print(f"time. generate joint distribution sample: {t_gen_samp}")
     print(f"time. fit joint distribution sample: {t_fit_samp}")
-# time, benchmark(normal distribution generate): 0.002388919999999999
-# time. generate joint distribution sample: 0.12924637
-# time. fit joint distribution sample: 9.89350620375
+# time, benchmark(normal distribution generate): 0.0023987495799999967
+# time. generate joint distribution sample: 0.13660708917
+# time. fit joint distribution sample: 9.71036204334
     for param in mi.parameters:
         value = mi.values[param]
         error = mi.errors[param]
         print(f"{param}, value: {value}, error: {error}")
-# N, value: 100000.11566607063, error: 316.2278705715917
-# mu, value: 2.995978939197134, error: 0.002547711444145466
-# sigma, value: 0.2980989755225555, error: 0.0024455735633328955
-# beta, value: 1.0144100338780004, error: 0.02188330129199616
-# m, value: 1.3503588518086418, error: 0.05831105257700519
-# f, value: 0.5951146318907781, error: 0.0035400929404916254
-# lamb, value: 0.2973744739614957, error: 0.002062927464454676
-# mu_b, value: 0.12313059108177564, error: 0.07245203573491325
-# sigma_b, value: 2.442622761880231, error: 0.034776180638041686
+# N, value: 100000.11575216857, error: 316.22786085488514
+# mu, value: 2.999763583264287, error: 0.002573160504685461
+# sigma, value: 0.2985989303031765, error: 0.0024306532652749457
+# beta, value: 1.0032515039172913, error: 0.02187017912555204
+# m, value: 1.4007626091157095, error: 0.061208043900852376
+# f, value: 0.6016274023544257, error: 0.003527987256229652
+# lamb, value: 0.3005819731561137, error: 0.002047073636828421
+# mu_b, value: 0.10786654508983966, error: 0.0732512247601983
+# sigma_b, value: 2.425277319467927, error: 0.0351125566939664
